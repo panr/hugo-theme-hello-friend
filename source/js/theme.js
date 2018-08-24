a8b843adb8d5f78aadf70e9d9452801cba699bfd
panr <radoslaw.koziel@gmail.com>
2018-08-24 09:37:55 +0200

    New Version - 1.0.1 — Set dark theme as default & fix eslint

--- source/js/theme.js
+++ source/js/theme.js
@@ -1,12 +1,16 @@
 // Toggle theme
 
-const getTheme = localStorage.getItem('theme')
+const getTheme = window.localStorage && window.localStorage.getItem('theme')
 const themeToggle = document.querySelector('.theme-toggle')
-const isDark = getTheme === 'dark'
+const isDark = getTheme === 'dark' || getTheme === null
 
 document.body.classList.toggle('dark-theme', isDark)
 
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme')
-  localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light')
+  window.localStorage &&
+    window.localStorage.setItem(
+      'theme',
+      document.body.classList.contains('dark-theme') ? 'dark' : 'light',
+    )
 })
