9532cb828522a9f63f20a84777b911be17b6f6ac
panr <radoslaw.koziel@gmail.com>
2018-12-21 19:07:37 +0100

    Set default theme in config toml

--- source/js/theme.js
+++ source/js/theme.js
@@ -1,16 +1,18 @@
 // Toggle theme
 
-const getTheme = window.localStorage && window.localStorage.getItem('theme')
-const themeToggle = document.querySelector('.theme-toggle')
-const isDark = getTheme === 'dark' || getTheme === null
+const getTheme = window.localStorage && window.localStorage.getItem("theme");
+const themeToggle = document.querySelector(".theme-toggle");
+const isDark = getTheme === "dark";
 
-document.body.classList.toggle('dark-theme', isDark)
+if (getTheme !== null) {
+  document.body.classList.toggle("dark-theme", isDark);
+}
 
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme')
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
   window.localStorage &&
     window.localStorage.setItem(
-      'theme',
-      document.body.classList.contains('dark-theme') ? 'dark' : 'light',
-    )
-})
+      "theme",
+      document.body.classList.contains("dark-theme") ? "dark" : "light",
+    );
+});
