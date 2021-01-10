d549a90e481b0080991a056792c4636ba8c45ece
Djordje Atlialp <djordje@atlialp.com>
2021-01-10 18:09:12 +0100

    Overwork background image support

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,11 +14,13 @@ if (theme !== null) {
 
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
+
   window.localStorage &&
     window.localStorage.setItem(
       "theme",
       document.body.classList.contains("dark-theme") ? "dark" : "light"
     );
+
   document.body.classList.contains("dark-theme")
     ? metaThemeColor.setAttribute("content", "#252627")
     : metaThemeColor.setAttribute("content", "#fafafa");
