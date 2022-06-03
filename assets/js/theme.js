e99e832d946e4741bdb30f0b9ee5b5b84e0b2423
panr <radoslaw.koziel@gmail.com>
2022-06-04 01:07:58 +0200

    Allow OS theme when no theme has been set

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,18 +1,31 @@
 // Toggle theme
 
-const getTheme = window.localStorage && window.localStorage.getItem("theme");
+const localTheme = window.localStorage && window.localStorage.getItem("theme");
 const themeToggle = document.querySelector(".theme-toggle");
-const isDark = getTheme === "dark";
 
-if (getTheme !== null) {
-  document.body.classList.toggle("dark-theme", isDark);
+if (localTheme) {
+  document.body.classList.remove("light-theme", "dark-theme");
+  document.body.classList.add(localTheme);
 }
 
 themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
+  const themeUndefined = !new RegExp("(dark|light)-theme").test(document.body.className);
+  const isOSDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  if (themeUndefined) {
+    if (isOSDark) {
+      document.body.classList.add("light-theme");
+    } else {
+      document.body.classList.add("dark-theme");
+    }
+  } else {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+  }
+
   window.localStorage &&
     window.localStorage.setItem(
       "theme",
-      document.body.classList.contains("dark-theme") ? "dark" : "light",
+      document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme",
     );
 });
